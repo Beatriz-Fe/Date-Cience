5bd6efa8ab228b3bc5ddf294b37cd54e167a3f7c
Misú <biaf0949@gmail.com>
2025-06-27 13:28:50 +0000

    Aula05

--- aula01.py
+++ aula01.py
@@ -1,1 +1,2 @@
-def exibirGrafico();
+def exibirGrafico(): 
+    
---
+++ aula02.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+aluno = ['Amanda', 'Caio', 'Carla', 'Diego', 'Fernanda']
+nota = [8.5, 6.0, 9.0, 7.5, 5.5]
+
+plt.bar(aluno, nota, color=['#520120', '#08403E', '#706513', '#B57114', '#962B09'])
+
+plt.title('Notas em Ciência de Dados')
+plt.xlabel('Aluno')
+plt.ylabel('Nota')
+
+plt.show()
+plt.savefig('exercicios/ex02.png')
