75fd03fdf9fbadcc57820cfe69003b1b4e23c482
Misú <biaf0949@gmail.com>
2025-09-03 19:29:56 +0000

    commit

--- aula05.py
+++ aula05.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 dados = [10, 20, 20, 30, 40, 50, 50, 50, 60, 70, 80, 90, 100]
 
 # Calcular quartis
-quartis = np.percentile(dados [25,50,75])
+quartis = np.percentile(dados, [25, 50, 75])
 print(f'Quartis: Q1={quartis[0]}, Q2={quartis[1]}, Q3={quartis[2]}')
 
 #calcular decis
