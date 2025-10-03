4947c12574fdfb8f48eb188ad8655112a232d6ab
Misú <biaf0949@gmail.com>
2025-10-03 13:53:15 +0000

    aula09 - Importando arquivo externo

--- aula08/aula08.py
+++ aula08/aula08.py
@@ -14,3 +14,19 @@ print ('-+-' * 50)
 #Fornecendo uma pré-analise dos dados para encontrar
 #correlações fortes positivas/negativas ou sem correlação
 print(dados.corr().round(2))
+
+sns.pairplot(dados)
+plt.show()
+plt.savefig('./aula08/pairplot.png')
+
+#Fazendo analise individual para correlação forte positiva
+#x -> Variavel dependente: Mato Grosso
+#Y -> Variavel descritiva: Rondonia
+sns.lmplot(data=dados, x="mato_grosso", y="rondonia")
+plt.show()
+
+plt.savefig('./aula08/lmplot-positivo.png')
+
+sns.jointplot(data=dados, x="referencia", y="tocantins", kind='reg')
+plt.show()
+plt.savefig('./aula08/jointplot-negativo.png')
